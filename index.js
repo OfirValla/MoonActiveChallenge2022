c91df5a228b5e2f8c005e74338433c047a37cc86
Ofir Valla <ofirvalla50@gmail.com>
2022-05-20 14:21:28 +0300

    Update the http server to be restana

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const fs = require('fs');;
-const app = require('express')();
+const app = require('restana')();
 const port = +process.argv[2] || 3000;
 
 const client = require('redis').createClient();
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 client.on('ready', () => {
-    app.listen(port, '0.0.0.0', () => {
+    app.start(port).then(server => {
         console.log(`Example app listening at http://0.0.0.0:${port}`);
     });
 });
 
-const defaultMessage = {id: 'ALL CARDS'};
+const defaultMessage = { id: 'ALL CARDS' };
 const cards = (() => {
     const output = JSON.parse(fs.readFileSync('./cards.json'));
-    output.unshift({id: 'NEVER USED'});
+    output.unshift({ id: 'NEVER USED' });
     return output;
 })();
 
 const getMissingCard = async (key) => cards[await client.incr(key)] || defaultMessage
 
-app.get('/card_add', async (req, res) => res.send(await getMissingCard(req.query.id)) );
+app.get('/card_add', async (req, res) => res.send(await getMissingCard(req.query.id)));
 
 app.get('/ready', async (req, res) => {
     await client.ping();
-    res.send({ready: true});
+    res.send({ ready: true });
 });
 
 client.connect();
